5e3be24558b770079f1a464b7667d34b6feb40aa
etctera <yilonglei790@gmail.com>
2023-12-04 23:56:39 +0800

    feat: calc ma5

--- with_polars/src/main.rs
+++ with_polars/src/main.rs
@@ -34,7 +34,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // 替换列名
     df.set_column_names(TABLE_HEADERS)?;
 
-    // 计算振幅
+    calc_amp(&mut df)?;
+
+    calc_ma5(&mut df)?;
+
+    // 使用 select 根据列名选择列，再使用 head 选择前 10 行
+    // df = df.select(["交易日期"])?.head(Some(10));
+
+    println!("{:?}", df);
+
+    Ok(())
+}
+
+fn calc_amp(df: &mut DataFrame) -> Result<(), PolarsError> {
+    // 获取列数
+    let len = df.width();
+
     let highest_price = df.column("最高价")?;
     let lowest_price = df.column("最低价")?;
     let previous_closing_price = df.column("前收盘价")?;
@@ -51,16 +66,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let amplitude = Series::new("振幅", amplitude);
 
-    //
-    let len = df.width();
-
     // 在尾部添加新列
     df.insert_column(len, amplitude)?;
 
-    // 使用 select 根据列名选择列，再使用 head 选择前 10 行
-    // df = df.select(["交易日期"])?.head(Some(10));
+    Ok(())
+}
 
-    println!("{:?}", df);
+fn calc_ma5(df: &mut DataFrame) -> Result<(), PolarsError> {
+    // 获取列数
+    let len = df.width();
+
+    let closing_price = df.column("收盘价")?;
+
+    let opt = RollingOptionsImpl {
+        min_periods: 5,
+        window_size: Duration::new(5),
+        ..Default::default()
+    };
+    let ma5 = closing_price.reverse().rolling_mean(opt)?;
+
+    let ma5 = Series::new("MA5", ma5);
+
+    // 在尾部添加新列
+    df.insert_column(len, ma5)?;
 
     Ok(())
 }
