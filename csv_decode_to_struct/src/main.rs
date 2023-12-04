1064cae63263d79941346378d4e239785e12bd60
etctera <yilonglei790@gmail.com>
2023-12-04 12:53:24 +0800

    feat: add struct Date type

--- csv_decode_to_struct/src/main.rs
+++ csv_decode_to_struct/src/main.rs
@@ -8,14 +8,26 @@ use std::io::Cursor;
 // 因为相对路径是根据执行目录来计算的，所以这里的 csv 文件路径是相对于执行目录的，放在子项目根目录读取不到，除非在该子目录下执行 cargo run
 // const CSV_PATH: &str = "./588800.SH.csv";
 // 要使用相对路径读取子项目的 csv 文件，则需要使用如下相对路径
-const CSV_PATH: &str = "./csv_read/588460.SH.csv";
+const CSV_PATH: &str = "./588460.SH.csv";
 
 #[derive(Default, Debug, Clone, Deserialize)]
 struct DataFrame {
     // 对应 csv 文件的列名
     #[serde(deserialize_with = "date_from_str")]
     trade_date: NaiveDate,
+    volume: Option<f64>,
+    turnover: Option<f64>,
+    fund_code: Option<String>,
+    lowest_price: Option<f64>,
+    nav_per_unit: Option<f64>,
+    highest_price: Option<f64>,
+    closing_price: Option<f64>,
+    opening_price: Option<f64>,
     stock_name: Option<String>,
+    turnover_rate: Option<f64>,
+    post_adjustment_factor: Option<f64>,
+    previous_closing_price: Option<f64>,
+    accumulated_nav_per_unit: Option<f64>,
 }
 
 fn date_from_str<'de, D>(deserializer: D) -> Result<NaiveDate, D::Error>
@@ -42,7 +54,8 @@ fn main() {
     // 读取 csv 数据，并反序列化为 struct
     rdr.deserialize().for_each(|r| {
         let record: DataFrame = r.unwrap();
-        println!("stock_name: {:?}", record.stock_name.unwrap());
-        println!("trade_date: {:?}", record.trade_date);
+        println!("record: {:?}", record);
+        // println!("stock_name: {:?}", record.stock_name.unwrap());
+        // println!("trade_date: {:?}", record.trade_date);
     });
 }
