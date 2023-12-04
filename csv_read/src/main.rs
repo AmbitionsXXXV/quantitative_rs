8913b54663c152c3c5d39242d0dd0c03c3f683fa
etctera <yilonglei790@gmail.com>
2023-12-04 09:19:30 +0800

    feat: csv_read

--- csv_read/src/main.rs
+++ csv_read/src/main.rs
@@ -6,7 +6,7 @@ use std::io::Cursor;
 // 因为相对路径是根据执行目录来计算的，所以这里的 csv 文件路径是相对于执行目录的，放在子项目根目录读取不到，除非在该子目录下执行 cargo run
 // const CSV_PATH: &str = "./588800.SH.csv";
 // 要使用相对路径读取子项目的 csv 文件，则需要使用如下相对路径
-const CSV_PATH: &str = "./csv_read/588800.SH.csv";
+const CSV_PATH: &str = "./csv_read/588460.SH.csv";
 
 fn main() {
     // 以二进制格式读取整个文件内容
@@ -23,6 +23,10 @@ fn main() {
 
     // 逐行读取CSV
     for sr in rdr.records().flatten() {
+        // 打印第一列数据
+        println!("row: {:?}", sr.get(0).unwrap());
+
+        // 打印所有列数据
         sr.into_iter().for_each(|c| println!("{}", c))
     }
 }
