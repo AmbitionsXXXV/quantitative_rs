cd304f5740417d288cab5e6c792483faeb15f68a
etctera <yilonglei790@gmail.com>
2023-12-05 18:01:08 +0800

    chore: code optim

--- with_polars_lazy/src/lib.rs
+++ with_polars_lazy/src/lib.rs
@@ -5,10 +5,15 @@ pub fn filter_csv_lazy(
     df: DataFrame,
     patron: &str,
 ) -> Result<DataFrame, Box<dyn std::error::Error>> {
+    let print_exp = [col("发布日期").alias("pub_date")];
+
     let g = df
         .lazy()
         .filter(col("股票代码").str().contains(lit(patron), false))
         .collect()?;
 
-    Ok(g)
+    // 增加一个 select 操作，只选择发布日期列
+    let ret = g.lazy().select(print_exp).collect()?;
+
+    Ok(ret)
 }
